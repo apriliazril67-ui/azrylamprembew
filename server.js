a7f519fc95d652ebb5cca4928fc86136ec6bd289
apriliazril67-ui <apriliazril67@gmail.com>
2026-08-28 02:50:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
 // Path database
 const DATA_DIR = process.env.NODE_ENV === 'production' 
   ? '/app/data' 
